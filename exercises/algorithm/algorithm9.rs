6a91074b23f0ad9535f54f81f3a400cd7c4cabe0
a6d9a6m <1840309785@qq.com>
2025-03-27 20:39:17 +0800

    111

--- exercises/algorithm/algorithm9.rs
+++ exercises/algorithm/algorithm9.rs
@@ -2,7 +2,6 @@
 	heap
 	This question requires you to implement a binary heap function
 */
-// I AM NOT DONE
 
 use std::cmp::Ord;
 use std::default::Default;
@@ -37,7 +36,20 @@ where
     }
 
     pub fn add(&mut self, value: T) {
-        //TODO
+        self.items.push(value);
+        self.count += 1;
+        let mut idx = self.count;
+
+        while idx > 1 {
+            let parent_idx = self.parent_idx(idx);
+            if (self.comparator)(&self.items[idx], &self.items[parent_idx]) {
+                self.items.swap(idx, parent_idx);
+                idx = parent_idx;
+            } else {
+                break;
+            }
+        }
+
     }
 
     fn parent_idx(&self, idx: usize) -> usize {
@@ -57,8 +69,20 @@ where
     }
 
     fn smallest_child_idx(&self, idx: usize) -> usize {
-        //TODO
-		0
+        let left = self.left_child_idx(idx);
+        let right = self.right_child_idx(idx);
+
+        // 检查右子节点是否存在
+        if right > self.count {
+            left  // 只有左子节点
+        } else {
+            // 根据 comparator 决定选择哪个子节点
+            if (self.comparator)(&self.items[left], &self.items[right]) {
+                left
+            } else {
+                right
+            }
+        }
     }
 }
 
@@ -84,8 +108,26 @@ where
     type Item = T;
 
     fn next(&mut self) -> Option<T> {
-        //TODO
-		None
+        if self.is_empty(){
+            return None;
+        }
+        let root = self.items.swap_remove(1);
+        self.count -= 1;
+
+        if self.count > 0{
+            let mut idx = 1;
+            while self.children_present(idx){
+                let child = self.smallest_child_idx(idx);
+                if (self.comparator)(&self.items[child], &self.items[idx]) {
+                    self.items.swap(idx, child);
+                    idx = child;
+                } else {
+                    break;
+                }
+            }
+        }
+        Some(root)
+
     }
 }
 
